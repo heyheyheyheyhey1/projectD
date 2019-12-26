2e40d4f0332e1d82aec4e204fc3a4d012d191e44
heyheyheyheyhey1 <912126472@qq.com>
2019-12-26 15:07:54 +0800

    project init

---
+++ src/main/java/com/jhx/projectd/ProjectdApplication.java
@@ -0,0 +1,13 @@
+package com.jhx.projectd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhx/projectd/ProjectdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhx.projectd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
